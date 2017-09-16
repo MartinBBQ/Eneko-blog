0af40b418d7ad675d41f49f422fcc8fe403c7e01
Ismaël Sebbane <ismael.sebbane@gmail.com>
2017-09-16 13:06:17 +0200

    Pick only the host on a path url (custom articles)

--- wp-content/themes/eneko/resources/views/partials/content.blade.php
+++ wp-content/themes/eneko/resources/views/partials/content.blade.php
@@ -17,6 +17,7 @@
            $siteName = $parsedUrl['host'];
         } else {
            $siteName = $parsedUrl['path'];
+           $siteName = explode('/',$siteName)[0];
         }
         $siteName = str_replace('www.','', $siteName);
         if(!$result instanceof WP_Error) {
@@ -36,24 +37,24 @@
 @endphp
 <article data-category="{{implode(' ',$termSlugs)}}" @php(post_class(\App\getArticleClasses()))>
     <a {{$isCustomArticle ? 'target="_blank"' : ''}} href="{{$url}}">
-    <div class="article__wrapper">
-        @if(!$isCustomArticle)
-        @foreach($termNames as $term)
-            @include('partials.content.buttonContainer', ['label' => $term])
-        @endforeach
-        @else
-            @include('partials.content.buttonContainer', ['label' => $siteName])
-        @endif
-        <h2 class="article__title">{{$title}}</h2>
-        <div class="article__bottom">
-            {{App\getArticleNameAndDate(true)}}
-        </div>
-    </div>
-    @php($thumb = get_the_post_thumbnail_url(get_post(),'full'))
-        @isset($thumb)
-            <div class="article__image" style="background-image: url({{$thumb}});">
-                <img src="{{$thumb}}" class="visually-hidden">
+        <div class="article__wrapper">
+            @if(!$isCustomArticle)
+                @foreach($termNames as $term)
+                    @include('partials.content.buttonContainer', ['label' => $term])
+                @endforeach
+            @else
+                @include('partials.content.buttonContainer', ['label' => $siteName])
+            @endif
+            <h2 class="article__title">{{$title}}</h2>
+            <div class="article__bottom">
+                {{App\getArticleNameAndDate(true)}}
             </div>
-        @endisset
+        </div>
+        @php($thumb = get_the_post_thumbnail_url(get_post(),'full'))
+            @isset($thumb)
+                <div class="article__image" style="background-image: url({{$thumb}});">
+                    <img src="{{$thumb}}" class="visually-hidden">
+                </div>
+            @endisset
     </a>
 </article>
