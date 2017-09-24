5d9ae19ba67bbb4f4631631b620d98f42953a1e1
Ismaël Sebbane <ismael.sebbane@gmail.com>
2017-09-24 16:29:50 +0200

    Changed sidebar order

--- wp-content/themes/eneko/resources/views/partials/sidebar.blade.php
+++ wp-content/themes/eneko/resources/views/partials/sidebar.blade.php
@@ -3,6 +3,44 @@
     $permaTitle = !empty(get_option('permanence')) ? get_option('permanence') : 'Permanences';
 @endphp
 <aside class="sidebar">
+    <div class="sidebar__group">
+        @php
+            $authorId = \App\getOwnerId();
+            $mail = get_the_author_meta('user_email', $authorId);
+        @endphp
+        <h3 class="sidebar__title">Contact</h3>
+        @php($loop = App\getCustomQuery(['post_type'=> 'employees', 'posts_per_page' => -1]))
+            @if($loop->have_posts())
+                @while ($loop->have_posts()) @php($loop->the_post())
+                    @if(get_field('isOnSidebar'))
+                        @include('partials.sidebar.contact')
+                    @endif
+                    @endwhile
+                    @endif
+                    {{wp_reset_query()}}
+    </div>
+    <div class="sidebar__group">
+        <div class="sidebar__info">
+            <a href="mailto:{{$mail}}">Écrire au député</a>
+        </div>
+        <div class="sidebar__info">
+            <a href="/propositions">Faire une proposition</a>
+        </div>
+    </div>
+        @if($loop->have_posts())
+            <div class="sidebar__group">
+                <h3 class="sidebar__title">{{$permaTitle}}</h3>
+                @while ($loop->have_posts()) @php($loop->the_post())
+                    @php
+                        $termSlug = get_the_terms(get_the_ID(), 'types')[0]->slug;
+                    @endphp
+                    @if($termSlug=='permanente')
+                        @include('partials.sidebar.permanence', ["isPermanent" => true])
+                    @endif
+                    @endwhile
+            </div>
+        @endif
+        {{wp_reset_query()}}
     @php($loop = App\getCustomQuery(['post_type'=> 'permanencies', 'posts_per_page' => -1]))
         @if($loop->have_posts())
             <div class="sidebar__group">
@@ -18,42 +56,4 @@
             </div>
         @endif
         {{wp_reset_query()}}
-        @if($loop->have_posts())
-            <div class="sidebar__group">
-                <h3 class="sidebar__title">{{$permaTitle}}</h3>
-                @while ($loop->have_posts()) @php($loop->the_post())
-                    @php
-                        $termSlug = get_the_terms(get_the_ID(), 'types')[0]->slug;
-                    @endphp
-                    @if($termSlug=='permanente')
-                        @include('partials.sidebar.permanence', ["isPermanent" => true])
-                    @endif
-                    @endwhile
-            </div>
-        @endif
-        {{wp_reset_query()}}
-        <div class="sidebar__group">
-            @php
-                $authorId = \App\getOwnerId();
-                $mail = get_the_author_meta('user_email', $authorId);
-            @endphp
-            <h3 class="sidebar__title">Contact</h3>
-            @php($loop = App\getCustomQuery(['post_type'=> 'employees', 'posts_per_page' => -1]))
-                @if($loop->have_posts())
-                    @while ($loop->have_posts()) @php($loop->the_post())
-                        @if(get_field('isOnSidebar'))
-                            @include('partials.sidebar.contact')
-                        @endif
-                        @endwhile
-                        @endif
-                        {{wp_reset_query()}}
-        </div>
-        <div class="sidebar__group">
-            <div class="sidebar__info">
-                <a href="mailto:{{$mail}}">Écrire au député</a>
-            </div>
-            <div class="sidebar__info">
-                <a href="/propositions">Faire une proposition</a>
-            </div>
-        </div>
 </aside>
