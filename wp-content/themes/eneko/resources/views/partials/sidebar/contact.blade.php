428a2f26f5378842fcc081ac727624655037092f
Ismaël Sebbane <ismael.sebbane@gmail.com>
2017-09-24 17:16:01 +0200

    Add role for contact in sidebar

--- wp-content/themes/eneko/resources/views/partials/sidebar/contact.blade.php
+++ wp-content/themes/eneko/resources/views/partials/sidebar/contact.blade.php
@@ -1,8 +1,15 @@
 @php
     $mail = get_field('mail');
     $phone = get_field('phone');
+    $category = get_the_terms(get_the_ID(),'roles');
+    if(!empty($category)) {
+        $categoryName = $category[0]->name;
+    } else {
+        $categoryName = '';
+    }
 @endphp
 <div class="contact">
+    <h5 class="contact__role">{{$categoryName}}</h5>
     <h5 class="contact__title">{{get_field('name')}}</h5>
     <div class="contact__group">
         <a href="tel:{{$phone}}" class="contact__info">{{$phone}}</a>
